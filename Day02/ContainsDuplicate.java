bf4a69d59affc215e52eaa913bf1b61bb96dd355
student umapathy <umapathyd907@gmail.com>
2026-09-11 22:45:17 +0530

    Day 11: Product of Array Except Self (Java) - Prefix/Suffix Products + gitignore cleanup

--- Day02/ContainsDuplicate.java
+++ Day02/ContainsDuplicate.java
@@ -1,3 +1,7 @@
+
+// LeetCode #217 - Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/
+
 import java.util.HashSet;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
--- Day08/LongestSubstringWithoutRepeating.java
+++ Day08/LongestSubstringWithoutRepeating.java
@@ -1,3 +1,7 @@
+
+// LeetCode #3 - Longest Substring Without Repeating Characters
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
 import java.util.HashSet;
 
 class Solution {
