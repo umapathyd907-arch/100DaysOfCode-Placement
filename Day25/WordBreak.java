8e7cbb657c48bd15786efdaf6e9a6fa7f3a13b7f
student umapathy <umapathyd907@gmail.com>
2026-09-25 20:41:04 +0530

    Day 25: Word Break (Java) - Bottom-Up DP + README update

---
+++ Day25/WordBreak.java
@@ -0,0 +1,23 @@
+// LeetCode #139 - Word Break
+// https://leetcode.com/problems/word-break/
+
+import java.util.*;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[s.length()];
+    }
+}
